dc3831fe9c1163286a1abd4ff6b5951d806aa0a2
Aiden <aidenrafferty74@gmail.com>
2023-11-04 21:36:56 -0700

    Deleted Comment

--- src/main/java/net/aiden/mccourse/MCCourseMod.java
+++ src/main/java/net/aiden/mccourse/MCCourseMod.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
     public static final Logger LOGGER = LoggerFactory.getLogger("mccourse");
-	//Test
+
 	@Override
 	public void onInitialize() {
 
